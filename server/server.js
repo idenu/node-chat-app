46cdc3d2a7f1c95087024909faba7dfd334e557c
idenu <benjamin@idenu.dk>
2018-07-10 16:37:36 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected!');
 
+    // socket.emit from admin text: "Welcome to the chap app"
+    socket.emit('newMsg', {
+        from: 'ADMIN',
+        text: 'Welcome to the chap app'
+    });
+
+    // socket.broadcast.emit text: "New user joined"
+    socket.broadcast.emit('newMsg', {
+        from: 'ADMIN',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMsg', (msg) => {
         console.log(msg.from);
         io.emit('newMsg', {
@@ -21,6 +34,11 @@ io.on('connection', (socket) => {
             text: msg.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMsg', {
+        //     from: msg.from,
+        //     text: msg.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
